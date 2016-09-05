02f1620a8a3ce6b6b5feae319e354573f01c29a0
Nevermore55 <wuyingjian555@gmail.com>
2016-09-05 21:20:34 +0800

    simulate html add field

--- src/app/pages/simulate/simulate.controller.js
+++ src/app/pages/simulate/simulate.controller.js
@@ -14,9 +14,46 @@ function SimulateCtrl(Simulate, AlertBox) {
     vm.moneyTo = '';
     vm.city = '';
     vm.amount = '';
+    vm.addNormalPanelFlag = true;
+    vm.addFraudPanelFlag = false;
+    vm.addRulePanelFlag = false;
 
     vm.submitnormal = submit;
+    init();
 
+
+    function init() {
+        initPicker();
+    }
+
+    function initPicker() {
+        $('.default-date-picker').datepicker({
+            format: 'yyyy-mm-dd'
+        });
+        var checkin = $('.dpd1').datepicker({
+            format: 'yyyy-mm-dd',
+            onRender: function(date) {
+                return date.valueOf() < now.valueOf() ? 'disabled' : '';
+            }
+        }).on('changeDate', function(ev) {
+            if (ev.date.valueOf() > checkout.date.valueOf()) {
+                var newDate = new Date(ev.date);
+                newDate.setDate(newDate.getDate() + 1);
+                checkout.setValue(newDate);
+            }
+            checkin.hide();
+            $('.dpd2')[0].focus();
+        }).data('datepicker');
+        var checkout = $('.dpd2').datepicker({
+            format: 'yyyy-mm-dd',
+            onRender: function(date) {
+                return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+            }
+        }).on('changeDate', function(ev) {
+            checkout.hide();
+        }).data('datepicker');
+    }
+    
     function submit() {console.log(vm);
         Simulate
             .add(vm)
